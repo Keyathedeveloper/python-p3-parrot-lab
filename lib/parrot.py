3a5ab05a600fec8e04e38c36ee589bbaf1da28d3
Ta'Keya McFadden <takeyamcfadden@gmail.com>
2024-03-06 09:08:18 -0500

    lab complete. all tests passing.

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,5 @@
-def parrot():
-    pass
+def parrot(message="Squawk!"):
+    print(message)
+    return message
+default_message = parrot()  # This will print "Squawk!" and assign "Squawk!" to default_message
+custom_message = parrot("Hello!")  # This will print "Hello!" and assign the same string to custom_message
